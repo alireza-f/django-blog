b707f0443026b4d6997ad50e31fbeae9a0510d23
alireza-f <alirezafathi.ce@gmail.com>
2021-08-14 02:20:53 +0430

    add picture upload

--- alirezaf/urls.py
+++ alirezaf/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
